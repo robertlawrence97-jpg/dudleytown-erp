7fc823feaa500bc79538213a51563ec06132b37f
robertlawrence97-jpg <robert.lawrence97@gmail.com>
2025-11-28 10:33:07 -0500

    Add files via upload

---
+++ dudleytown-erp/client/src/pages/production/BatchesList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box, Typography, Paper } from '@mui/material';
+
+const BatchesList: React.FC = () => {
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography variant="h4" gutterBottom>
+        Batches List
+      </Typography>
+      <Paper sx={{ p: 3, mt: 2 }}>
+        <Typography variant="body1" color="text.secondary">
+          This feature is coming soon.
+        </Typography>
+      </Paper>
+    </Box>
+  );
+};
+
+export default BatchesList;
